46b489962b1d62639e8a8c38b8d1ac9c900be7ba
josed325 <jose.erazo3@catolica.edu.sv>
2024-02-24 02:03:58 -0600

    Cambios Finales

--- WebApiPracticas/Controllers/equiposController.cs
+++ WebApiPracticas/Controllers/equiposController.cs
@@ -32,8 +32,125 @@ namespace WebApiPracticas.Controllers
 
             return Ok(listadoEquipo);
         }
+        /// <summary>
+        /// EndPoint que retorna los registros de una tabla filtrados por su ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos 
+                               where e.id_equipos == id 
+                               select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(equipo);
+        }
+
+        /// <summary>
+        /// EndPoint que retorna los registros de todos los equipos existentes 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Find/{filtro}")]
+
+        public IActionResult FindByDescription(string filtro)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.description.Contains(filtro)
+                               select e).FirstOrDefault();
+
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return Ok(equipo);
+        }
+
+        [HttpGet]
+        [Route("Add")]
+
+        public IActionResult GuadarEquipo([FromBody] equipos equipo)
+        {
+
+            try
+            {
+
+                _equiposContexto.equipos.Add(equipo);
+                _equiposContexto.SaveChanges();
+                return Ok(equipo);
+
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+
+
+        }
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar)
+        {
+            //Para actualizar un registro, se obtiene el registro original de la base de datos
+            //al cual alteraremos alguna propiedad
+            equipos? equipoActual = (from e in _equiposContexto.equipos
+                                     where e.id_equipos == id
+                                     select e).FirstOrDefault();
+
+            //Verificamos que exista el registro segun su ID
+            if (equipoActual == null)
+            { return NotFound(); }
+
+            //Si se encuentra el registro, se alteran los campos modificables 
+            equipoActual.nombre = equipoModificar.nombre;
+            equipoActual.description = equipoModificar.description;
+            equipoActual.marca_id = equipoModificar.marca_id;
+            equipoActual.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+            equipoActual.anio_compra = equipoModificar.anio_compra;
+            equipoActual.costo = equipoModificar.costo;
+
+            //Se marca el registro como modificado en el contexto
+            //y se envia la modificacion a la base de datos
+
+            _equiposContexto.Entry(equipoActual).State = EntityState.Modified;
+            _equiposContexto.SaveChanges();
+
+            return Ok(equipoModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+
+        public IActionResult EliminarEquipo(int id)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+
+            if (equipo == null)
+            {
+                return NotFound();
+
+
+                _equiposContexto.equipos.Attach(equipo);
+                _equiposContexto.equipos.Remove(equipo);
+                _equiposContexto.SaveChanges();
+
+            }
+            return Ok(equipo);
+        }
 
     }
+
 
 
 }
